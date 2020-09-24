27d90a64e1f01aba1c6983e73dedec366557a0fa
Robin <titanen.197@hotmail.com>
2020-09-24 15:33:01 +0200

    map opdracht

--- 02-FilesFolders/huidige_map.py
+++ 02-FilesFolders/huidige_map.py
@@ -5,14 +5,26 @@ werkmap = os.getcwd()
 print("De huidige map is: " + werkmap)
 
 
-mapnaam = input("Welke naam wil je voor de map")
 
-lengte_mapnaam = len(mapnaam)
-if lengte_mapnaam > 0:
-    os.mkdir(mapnaam)
-    print("De map " + mapnaam + " is gemaakt.")
-else:
-    print("Je hebt geen naam ingevoerd")
+mapnaam = ""
+
+lengte_mapnaam = 0
+
+while lengte_mapnaam == 0:
+    mapnaam = input("Welke naam wil je voor de map")
+
+# Sla de lengte van de mapnaam op in de variabele lengte_mapnaam
+    lengte_mapnaam = len(mapnaam)
+
+
+# Sla de lengte van de mapnaam op in de variabele lengte_mapnaam
+    if lengte_mapnaam > 0:
+        os.mkdir(mapnaam)
+    else:
+# En anders melden we dat er niets is ingevuld
+        print("Je hebt geen naam ingevoerd")
     
 
+# ... en anders gaat de code hier verder
 print("De map " + mapnaam + " is gemaakt.")
+
